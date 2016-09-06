bd1a671abdbbf3a3e1fe13b86f61ea70b3e56d2f
fdj <janviernguyen@gmail.com>
2016-09-06 08:29:47 +0700

    add map constructor and map constructor editor

---
+++ Assets/001_Scripts/Editor/MapConstructorEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapConstructor))]
+public class MapConstructorEditor : Editor {
+	public override void OnInspectorGUI (){
+		// DrawDefaultInspector();
+		var script = target as MapConstructor;
+
+		script.mapId = EditorGUILayout.IntField ("Map Id", script.mapId);
+		script.mapName = EditorGUILayout.TextField ("Map Name", script.mapName);
+
+		#region tower
+		EditorGUILayout.LabelField("Tower Point");
+		EditorGUILayout.BeginHorizontal ();
+		if (GUILayout.Button ("Create Tower Point")) {
+			Debug.Log ("create a tower point");
+
+		}
+		if (GUILayout.Button ("Remove Tower Point")) {
+			Debug.Log ("remove a tower point");
+		}
+		EditorGUILayout.EndHorizontal();
+		#endregion tower
+
+		#region waypoint 
+		EditorGUILayout.LabelField("Way Point");
+		EditorGUILayout.BeginHorizontal ();
+		if (GUILayout.Button ("Create Way Point")) {
+			Debug.Log ("create a way point");
+		}
+		if (GUILayout.Button ("Remove Way Point")) {
+			Debug.Log ("remove a way point");
+		}
+		EditorGUILayout.EndHorizontal();
+		#endregion waypoint
+	}
+}
---
+++ Assets/001_Scripts/MapConstructor.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MapConstructor : MonoBehaviour {
+
+    // map id
+    public int mapId;
+    // public int MapId
+    // {
+    //     get
+    //     {
+    //         return mapId;
+    //     }
+
+    //     set
+    //     {
+    //         mapId = value;
+    //     }
+    // }
+
+    // map name
+    public string mapName;
+    // public string MapName
+    // {
+    //     get
+    //     {
+    //         return mapName;
+    //     }
+
+    //     set
+    //     {
+    //         mapName = value;
+    //     }
+    // }
+
+    // wave datas
+    public struct WaveData {
+		EnemyType type;
+		int amount;
+		float interval;
+	}
+	public List <WaveData> waveDatas;
+    
+	// tower point 
+	public List<Vector3> towerPoints;
+    
+	// way point
+	public List<Vector3> wayPoints;
+
+
+	// TODO: SAVE AND LOAD MAP DATA
+
+}
