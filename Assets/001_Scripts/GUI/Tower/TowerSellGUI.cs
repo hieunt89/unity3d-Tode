757eed8bde84e979b6cab7661b6012a4fd10952c
Tan Minh <m.tan2007@gmail.com>
2016-09-27 09:06:29 +0700

    nothing much

---
+++ Assets/001_Scripts/GUI/Tower/TowerSellGUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Entitas;
+
+public class TowerSellGUI : MonoBehaviour {
+
+
+	void Start(){
+		Messenger.AddListener (Events.Input.EMPTY_CLICK, ClearTowerSellBtn);
+		Messenger.AddListener <Entity> (Events.Input.ENTITY_CLICK, RegisterTowerSellBtn);
+	}
+
+	void OnDestroy(){
+		Messenger.RemoveListener (Events.Input.EMPTY_CLICK, ClearTowerSellBtn);
+		Messenger.RemoveListener <Entity> (Events.Input.ENTITY_CLICK, RegisterTowerSellBtn);
+	}
+
+	public void ClearTowerSellBtn(){
+		
+	}
+
+	public void RegisterTowerSellBtn(Entity e){
+		
+	}
+}
