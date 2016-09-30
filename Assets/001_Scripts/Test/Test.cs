f2604d3dbed81e9838b55f213e76f4dec24c09a3
hieunt <hieu.nguyen@taplifeapp.com>
2016-09-30 09:58:37 +0700

    test parse json tree data

--- Assets/001_Scripts/Test/Test.cs
+++ Assets/001_Scripts/Test/Test.cs
@@ -1,24 +1,51 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class Data <T> where T : class{
+	public ChildData <T> rootData;
+
+	public Data (T data)
+	{
+		this.rootData = new ChildData<T> (data, null, new List<ChildData<T>>());
+	}
+	
+}
+
+[Serializable]
+public class ChildData <T> where T : class {
+	public T data;
+	public ChildData <T> parentData;
+	public List <ChildData<T>> childDatas;
+
+	public ChildData (T childData, ChildData<T> parentData, List<ChildData<T>> childDatas)
+	{
+		this.data = childData;
+		this.parentData = parentData;
+		this.childDatas = childDatas;
+	}
+
+	public void AddChildData (ChildData<T> _childData) {
+		childDatas.Add (_childData);
+	}
+
+	public void AddRelationship
+}
 
 public class Test : MonoBehaviour {
-	public Material mat;
+
 	void Start () {
-		OnPostRender ();
-	}
-	void OnPostRender() {
-		if (!mat) {
-			Debug.LogError("Please Assign a material on the inspector");
-			return;
-		}
-		GL.PushMatrix();
-		mat.SetPass(0);
-		GL.LoadOrtho();
-		GL.Begin(GL.TRIANGLES);
-		GL.Vertex3(0, 0, 0);
-		GL.Vertex3(1, 1, 0);
-		GL.Vertex3(0, 1, 0);
-		GL.End();
-		GL.PopMatrix();
+		Data<string> data = new Data<string> ("this is root data");
+
+		Debug.Log (data.rootData.data);
+		Debug.Log (data.rootData.childDatas.Count);
+		Debug.Log (JsonUtility.ToJson (data.rootData));
+
+		ChildData<string> newChildData = new ChildData<string> ("this is new child data", data.rootData, new List<ChildData<string>> ());
+
+		data.rootData.AddChildData (newChildData);
+
+		Debug.Log (JsonUtility.ToJson (data.rootData.childDatas));
 	}
 }
--- Assets/001_Scripts/TreeNode/Data/Tree.cs
+++ Assets/001_Scripts/TreeNode/Data/Tree.cs
@@ -5,12 +5,6 @@ using System.Collections.Generic;
 public class Tree <T> where T : class {
 	public Node <T> Root;
 
-//	public Tree (TreeType treeType, string treeName){
-//		this.treeType = treeType;
-//		this.treeName = treeName;
-//		this.Root = new Node<T> ();
-//	}
-//
 	public Tree (T rootData)
 	{
 		this.Root = new Node<T> (rootData);
